c4f2f203e7e9492ecbd9f9b24546548b9c7c04e5
Will Liang <willjhliang@gmail.com>
2022-12-11 21:37:38 -0500

    moved search in homepage

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import TinderCard from 'react-tinder-card'
 // import { useNavigate } from "react-router-dom";
-import { AiOutlineSearch } from 'react-icons/ai';
+import { AiOutlineSearch, AiFillHome } from 'react-icons/ai';
 
 import Card from "./Card";
 
@@ -18,18 +18,20 @@ function Homepage() {
 
     return (
         <div className="w-full h-screen flex flex-col items-center bg-opacity-10 bg-gradient-to-r from-indigo-200 via-purple-300 to-pink-200 p-24">
-            <div className="relative">
-                <TinderCard onSwipe={onSwipe} onCardLeftScreen={() => onCardLeftScreen('fooBar')} preventSwipe={['up', 'down']} className="w-[400px] h-[600px] bg-white select-none rounded shadow-xl hover:shadow-2xl ease-in absolute transition-shadow">
-                    <Card />
-                </TinderCard>
-                {/* <TinderCard onSwipe={onSwipe} onCardLeftScreen={() => onCardLeftScreen('fooBar')} preventSwipe={['right', 'left']} className="w-[400px] h-[600px] bg-white select-none rounded shadow-xl hover:shadow-2xl ease-in transition-shadow absolute top-0">
-                    <Card />
-                </TinderCard> */}
-                <div className="w-[400px] h-[600px]"></div>
+            <div className="w-5/6 flex flex-row justify-center space-x-10">
+                <AiFillHome size={30} style={{color: "white"}} />
+                <AiOutlineSearch size={30} style={{color: "white"}} />
             </div>
-            <div className="w-[600px] h-16 flex flex-row items-center justify-center space-x-4 bg-white mt-10 p-4 rounded-full opacity-90 shadow hover:shadow-2xl ease-in transition-shadow">
-                <AiOutlineSearch size={30} />
-                <p className="text-lg">Looking for a specific song, artist, album, genre, or country?</p>
+            <div className="flex h-full align-middle">
+                <div className="m-auto">
+                    <TinderCard onSwipe={onSwipe} onCardLeftScreen={() => onCardLeftScreen('fooBar')} preventSwipe={['up', 'down']} className="w-[400px] h-[600px] bg-white select-none rounded shadow-xl hover:shadow-2xl ease-in absolute transition-shadow">
+                        <Card />
+                    </TinderCard>
+                    {/* <TinderCard onSwipe={onSwipe} onCardLeftScreen={() => onCardLeftScreen('fooBar')} preventSwipe={['right', 'left']} className="w-[400px] h-[600px] bg-white select-none rounded shadow-xl hover:shadow-2xl ease-in transition-shadow absolute top-0">
+                        <Card />
+                    </TinderCard> */}
+                    <div className="w-[400px] h-[600px]"></div>
+                </div>
             </div>
         </div>
     );
