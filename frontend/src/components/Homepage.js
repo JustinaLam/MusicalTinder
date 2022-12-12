356c218d5816232611fe1ac172e8b9a609195a9a
charisgao <charis.m.gao@gmail.com>
2022-12-11 23:02:59 -0500

    search ranges, cardstack

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import TinderCard from 'react-tinder-card'
 // import { useNavigate } from "react-router-dom";
 import { AiOutlineSearch } from 'react-icons/ai';
 
 import Card from "./Card";
+import { getDefaultPopularSongs } from "../endpoints";
 
 function Homepage() {
     // const navigate = useNavigate();
+
+    const [songs, setSongs] = useState([]);
+
+    useEffect(() => {
+        async function fetchSongs() {
+        const data = await getDefaultPopularSongs();
+        setSongs(data);
+        }
+        fetchSongs();
+    }, []);
 
     const onSwipe = (direction) => {
       console.log('You swiped: ' + direction)
@@ -19,17 +30,16 @@ function Homepage() {
     return (
         <div className="w-full h-screen flex flex-col items-center bg-opacity-10 bg-gradient-to-r from-indigo-200 via-purple-300 to-pink-200 p-24">
             <div className="relative">
-                <TinderCard onSwipe={onSwipe} onCardLeftScreen={() => onCardLeftScreen('fooBar')} preventSwipe={['up', 'down']} className="w-[400px] h-[600px] bg-white select-none rounded shadow-xl hover:shadow-2xl ease-in absolute transition-shadow">
-                    <Card />
-                </TinderCard>
-                {/* <TinderCard onSwipe={onSwipe} onCardLeftScreen={() => onCardLeftScreen('fooBar')} preventSwipe={['right', 'left']} className="w-[400px] h-[600px] bg-white select-none rounded shadow-xl hover:shadow-2xl ease-in transition-shadow absolute top-0">
-                    <Card />
-                </TinderCard> */}
+                {songs.map((song) => 
+                    <TinderCard key={song.track_id} onSwipe={onSwipe} onCardLeftScreen={() => onCardLeftScreen('fooBar')} preventSwipe={['up', 'down']} className="w-[400px] h-[600px] bg-white select-none rounded shadow-xl hover:shadow-2xl ease-in absolute transition-shadow">
+                        <Card song={song} />
+                    </TinderCard>
+                )}
                 <div className="w-[400px] h-[600px]"></div>
             </div>
             <div className="w-[600px] h-16 flex flex-row items-center justify-center space-x-4 bg-white mt-10 p-4 rounded-full opacity-90 shadow hover:shadow-2xl ease-in transition-shadow">
                 <AiOutlineSearch size={30} />
-                <p className="text-lg">Looking for a specific song, artist, album, genre, or country?</p>
+                <input className="w-[500px]" placeholder="Looking for a specific song, artist, album, genre, or country?"></input>
             </div>
         </div>
     );
